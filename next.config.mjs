9e85f42fb16459e6a6c9467f97b46eb02c2d1c7f
Simret <simruineb@gmail.com>
2026-06-08 13:43:51 +0300

    Fix Next.js rewrite env variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,21 @@
-/** @type {import('next').NextConfig} */
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  'https://a2c-backend.oanstaging.com';
+
 const nextConfig = {
   reactStrictMode: true,
+
   sassOptions: {
     includePaths: ['./src/assets/styles'],
   },
+
   async rewrites() {
-    return {
-      fallback: [
-        {
-          source: '/api/:path*',
-          destination: `${API_BASE_URL}/api/:path*`,
-        },
-      ],
-    };
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${API_BASE_URL}/api/:path*`,
+      },
+    ];
   },
 };
 
